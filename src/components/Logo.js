600a44e52038153e3bbc115d1bcc7d7df551fb66
Selman Yıldız <selmanyildiz555@gmail.com>
2025-01-15 12:53:42 +0300

    logo

---
+++ src/components/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Logo = () => (
+  <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="25" cy="25" r="23" stroke="#48bb78" strokeWidth="4"/>
+    <path d="M15 25 H22 L25 15 L28 35 L31 25 H37" 
+      stroke="#48bb78" 
+      strokeWidth="4" 
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default Logo; 
